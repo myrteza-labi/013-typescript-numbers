b323a1d9ef7c46d3d1f28e214904bfb17c769245
myrteza <myrteza@hotmail.fr>
2023-10-07 16:28:23 +0200

    project done

---
+++ index.js
@@ -0,0 +1,19 @@
+var price;
+var price2 = 10.20;
+//Decimales numbers
+var counter = 9;
+var x = 100, y = 200;
+//Binary numbers 
+var bin = 2;
+var anotherBin = 2;
+// Octal numbers
+var octalNum = 64;
+var octalNul2 = 7;
+//hexadecimal numbers 
+var hexadecimalNum = 0xA;
+//Big integer 
+var big = 9007199254740991n;
+/*
+Javascript has the Number type.
+We should not use this Number type as much as possible in TypeScript.
+*/ 
---
+++ index.ts
@@ -0,0 +1,27 @@
+let price: number; 
+
+let price2 = 10.20; 
+
+//Decimales numbers
+let counter = 9; 
+
+let x : number = 100, y : number = 200; 
+
+//Binary numbers 
+let bin = 0b0010; 
+
+let anotherBin : number = 0b10; 
+
+// Octal numbers
+let octalNum : number = 0o100; 
+let octalNul2 : number = 0o7; 
+
+//hexadecimal numbers 
+let hexadecimalNum : number = 0xA; 
+
+//Big integer 
+let big: bigint = 9007199254740991n;
+/*
+Javascript has the Number type. 
+We should not use this Number type as much as possible in TypeScript.
+*/
